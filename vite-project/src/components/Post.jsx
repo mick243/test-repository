f1654736ede7027af98c19f632ca705ec84f6c2d
mick243 <kjm3746@gmail.com>
2026-06-10 16:47:11 +0900

    스타일링

--- vite-project/src/components/Post.jsx
+++ vite-project/src/components/Post.jsx
@@ -1,8 +1,21 @@
+// function Post(props) {
+//   return (
+//     <div>
+//       <p>{props.name}</p>
+//       <p>{props.body}</p>
+//     </div>
+//   );
+// }
+
+// export default Post;
+
+import styles from "./Post.module.css";
+
 function Post(props) {
   return (
-    <div>
-      <p>{props.name}</p>
-      <p>{props.body}</p>
+    <div className={styles.post}>
+      <p className={styles.author}>{props.name}</p>
+      <p className={styles.text}>{props.body}</p>
     </div>
   );
 }
