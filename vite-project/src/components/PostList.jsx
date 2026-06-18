af4faaf5f9436bddfdcd1a3ab89f1985036f91dc
mick243 <kjm3746@gmail.com>
2026-06-18 17:16:32 +0900

    텍스트 입력 실시간 반영

--- vite-project/src/components/PostList.jsx
+++ vite-project/src/components/PostList.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
+
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostList.module.css";
 
 function PostList() {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
       <ul className={classes.posts}>
-        <Post name="abc" body="aaaa" />
+        <Post name={enteredAuthor} body={enteredBody} />
         <Post name="def" body="bbbb" />
       </ul>
     </>
